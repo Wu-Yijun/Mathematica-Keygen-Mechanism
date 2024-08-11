aff9b0c883989dbd5400508f9a9deb79324e90bf
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2024-08-11 09:56:48 +0800

    fix minor bugs

--- index.js
+++ index.js
@@ -11,6 +11,61 @@ if (localStorage['user-agreement'] &&
   hideUserAgreement(localStorage['user-agreement']);
 }
 
+function cyrb128(str) {
+  let h1 = 1779033703, h2 = 3144134277, h3 = 1013904242, h4 = 2773480762;
+  for (let i = 0, k; i < str.length; i++) {
+    k = str.charCodeAt(i);
+    h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+    h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+    h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+    h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+  }
+  h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
+  h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
+  h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
+  h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
+  h1 ^= (h2 ^ h3 ^ h4), h2 ^= h1, h3 ^= h1, h4 ^= h1;
+  return [h1 >>> 0, h2 >>> 0, h3 >>> 0, h4 >>> 0];
+}
+
+function sfc32(a, b, c, d) {
+  return function() {
+    a |= 0;
+    b |= 0;
+    c |= 0;
+    d |= 0;
+    let t = (a + b | 0) + d | 0;
+    d = d + 1 | 0;
+    a = b ^ b >>> 9;
+    b = c + (c << 3) | 0;
+    c = (c << 21 | c >>> 11);
+    c = c + t | 0;
+    return (t >>> 0);
+  }
+}
+
+/**
+ * Generate random number from seed
+ *
+ * @param {String} seed  The seed to generate random number
+ * @returns {Number}  The random number
+ */
+function random_function(seed) {
+  var hashs = cyrb128(JSON.stringify(seed));
+  var seed_fun = sfc32(hashs[0], hashs[1], hashs[2], hashs[3]);
+  var count = seed_fun() % 100;
+  var random = seed_fun();
+  for (var i = 0; i < count; i++) {
+    random ^= seed_fun();
+  }
+  return random;
+}
+
+const today = new Date();
+if (random_function(today.toDateString()) % 2 == 0) {
+  document.body.style.backgroundColor = 'azure';
+}
+
 function copy(id) {
   const text = document.getElementById(id).innerText;
   navigator.clipboard.writeText(text)
